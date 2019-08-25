7b25ac25bd4ba50d21fe978faeca3575e9f8774c
twdeveloper <twdeveloper@outlook.com>
2019-08-25 22:24:10 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/linebotController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class linebotController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult POST()
+        {
+            string ChannelAccessToken = "___換成你的LINE_Bot的Channel_Access_Token___";
+            string YourUserID = "___換成Your_User_ID____";
+
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message;
+                Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(
+                    ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                isRock.LineBot.Utility.PushMessage(YourUserID, $"錯誤: {ex.Message}", ChannelAccessToken);
+                //請自行處理Exception
+                return Ok();
+            }
+        }
+    }
+}
